0e6b1bd65b96abf29eaf45f1795ef5867a85f507
Cainã <cainabarroso@gmail.com>
2025-12-09 21:12:41 -0300

    Acertando tratamento de erros

--- client_tcp.py
+++ client_tcp.py
@@ -4,13 +4,10 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Primeira parâmetro
 
 try:
     client.connect(("127.0.0.1", 4466)) # Função para conectar, primeiro parâmentro é o ip e o segundo é a porta
+    client.send(b"Oiii")# O "b" converte a string em bytes.
+    pacotes_recebidos = client.recv(1024).decode()# O decode converte os bytes em string
+    print(pacotes_recebidos)
 except Exception as error:
     print("Ocorreu um erro na conexão.")
     print(error)
-    exit()
 
-client.send(b"Oiii")# O "b" converte a string em bytes.
-
-pacotes_recebidos = client.recv(1024).decode()# O decode converte os bytes em string
-
-print(pacotes_recebidos)
